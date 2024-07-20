75d4c473f68e143cc6669eb5085d05cb9e5c1d0d
zxdscsfm <105771365+zxdscsfm@users.noreply.github.com>
2024-07-20 20:34:35 +0800

    Add files via upload

---
+++ mall_vue/src/views/manage/Home.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-07-15 12:26:05
+-->
+<template>
+    <div style="text-align: center;width: 100%;font-size: 32px;">
+      <h1 style="text-shadow: 1px 1px 1px white, -1px -1px 1px white, 1px -1px 1px white, -1px 1px 1px white;">欢迎访问在线商城后台管理系统</h1>
+    </div>
+    </template>
+    
+    <script>
+    import echarts from 'echarts'
+    export default {
+      name: "Home.vue"
+    }
+    </script>
+    
+    <style scoped>
+    </style>
