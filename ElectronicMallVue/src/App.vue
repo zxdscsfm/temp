cf30280202ade2e0e5f815788c4118fa994fb099
zxdscsfm <105771365+zxdscsfm@users.noreply.github.com>
2024-07-20 20:22:26 +0800

    Add files via upload

---
+++ ElectronicMallVue/src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-07-14 12:27:05
+-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+@import "./resource/css/icon.css";
+</style>
+
+
